64d0439d340ef36fd9d82540723e3e2d8ed015b7
Unknown <dani_mrx93@hotmail.com>
2018-06-21 22:27:15 -0300

    Add MainMenu and InputScene

---
+++ Assets/Scripts/Scenes/InputScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
---
+++ Assets/Scripts/Scenes/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject creditsPanel;
+
+    private void Start()
+    {
+        OnCloseCreditsButton();
+    }
+
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene("InputScene");
+    }
+
+    public void OnCreditsButton()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void OnCloseCreditsButton()
+    {
+        creditsPanel.SetActive(false);
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+    }
+
+}
